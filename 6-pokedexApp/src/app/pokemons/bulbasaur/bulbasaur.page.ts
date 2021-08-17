8265cb28454c82ca323c647650a27dbb5c124a7b
Everton L. de Souza <evertonldesouza@outlook.com>
2021-08-16 22:22:56 -0300

    correções page

--- 6-pokedexApp/src/app/pokemons/bulbasaur/bulbasaur.page.ts
+++ 6-pokedexApp/src/app/pokemons/bulbasaur/bulbasaur.page.ts
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BulbasaurPage implements OnInit {
 
-  constructor() { }
+  listaPokemon = [
+    {
+      numero: '001',
+      nome: 'Bulbasaur',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+      tipos: [
+        'Grass', 'Poison'
+      ],
+      fraquezas: [
+        'Fire', 'Physic', 'Flying', 'Ice'
+      ],
+      altura: 0.7,
+      peso: 6.9,
+      categoria: 'Semente',
+      habilidades: 'Overgrow'
+    }
+  ];
+
+  listaPokemonFiltrada = [];
+
+  constructor() {
+    this.retornarPokemon();
+  }
+
+  retornarPokemon(): void{
+    this.listaPokemonFiltrada = this.listaPokemon;
+  }
 
   ngOnInit() {
   }
